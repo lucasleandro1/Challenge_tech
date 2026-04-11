0f65d3f360473cdb827180bb041588dcf7328bfe
Lucas Oliveira <lukkassoliveira2215@gmail.com>
2026-04-11 16:05:30 -0300

    feat: add CrawlerService to scrape quotes.toscrape.com
    - Fetches first page of results for a given tag
    - Parses quote text, author name, author profile URL and tags
    - Returns array of hashes matching the API response format
    - Uses HTTParty for HTTP requests and Nokogiri for HTML parsing

---
+++ app/services/crawler_service.rb
@@ -0,0 +1,34 @@
+class CrawlerService
+  BASE_URL = "http://quotes.toscrape.com/tag"
+
+  def initialize(tag)
+    @tag = tag
+  end
+
+  def call
+    response = HTTParty.get("#{BASE_URL}/#{@tag}")
+    return [] unless response.success?
+
+    parse(response.body)
+  end
+
+  private
+
+  def parse(html)
+    doc = Nokogiri::HTML(html)
+
+    doc.css(".quote").map do |node|
+      {
+        "quote"        => node.css(".text").text.gsub(/\u201c|\u201d/, "").strip,
+        "author"       => node.css(".author").text.strip,
+        "author_about" => build_author_url(node),
+        "tags"         => node.css(".tag").map(&:text)
+      }
+    end
+  end
+
+  def build_author_url(node)
+    path = node.css("a").find { |a| a["href"]&.include?("/author/") }&.[]("href")
+    path ? "http://quotes.toscrape.com#{path}" : ""
+  end
+end
