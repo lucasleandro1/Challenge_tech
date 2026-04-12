4c47114c618b27fabf223f3c7b384f8156bc3a28
Lucas Oliveira <lukkassoliveira2215@gmail.com>
2026-04-12 14:04:00 -0300

    fix docker redis port and sidekiq cron setup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,17 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0") }
 
-  schedule = [
-    {
-      "name"  => "refresh_quotes_job",
-      "cron"  => "0 0,12 * * *",
-      "class" => "RefreshQuotesJob"
-    }
-  ]
+  config.on(:startup) do
+    schedule = [
+      {
+        "name"  => "refresh_quotes_job",
+        "cron"  => "0 0,12 * * *",
+        "class" => "RefreshQuotesJob"
+      }
+    ]
 
-  Sidekiq::Cron::Job.load_from_array!(schedule)
+    Sidekiq::Cron::Job.load_from_array!(schedule)
+  end
 end
 
 Sidekiq.configure_client do |config|
