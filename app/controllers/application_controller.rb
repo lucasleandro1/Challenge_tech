f611d6b0dbb381adf9879ff4c9fd36799e2c14c7
Lucas Oliveira <lukkassoliveira2215@gmail.com>
2026-04-15 14:18:21 -0300

    fix auth bypass when token is nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::API
 
   def authenticate_user!
     token = request.headers["Authorization"]&.split(" ")&.last
+    return render json: { error: "Unauthorized" }, status: :unauthorized if token.blank?
+
     @current_user = User.where(access_token: token).first
     render json: { error: "Unauthorized" }, status: :unauthorized unless @current_user
   end
